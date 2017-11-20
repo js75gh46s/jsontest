d338ffbdcda9b6d77b1b03f2a66b6b7cfbf8af89
js75gh46s <js75gh46s>
2017-11-20 14:02:22 -0500

    run karma in travis

---
+++ runKarma.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sbt jsontest-sharedJS/test:npmUpdate jsontest-sharedJS/test:buildKarma
+cd shared\js\target\scala-2.12\scalajs-bundler\test
+node .\node_modules\karma\bin\karma start karma.conf.js --single-run
